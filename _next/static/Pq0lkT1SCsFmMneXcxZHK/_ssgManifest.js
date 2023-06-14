554b866eb3170c99638a85fc47907fdaf53df708
rootEnginear <suthep.chanchuphol@outlook.com>
2023-06-14 09:36:26 +0000

    Update gh-pages to output generated at c91ffe9

---
+++ _next/static/Pq0lkT1SCsFmMneXcxZHK/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fbusiness\u002F[name]","\u002Fparty\u002F[name]","\u002Fperson\u002F[name]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/app/(info)/business/[name]/page-ecc9e77043cb99e1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[660,797,364,520,408],{8916:function(){}},function(n){n.O(0,[498,362,744],function(){return n(n.s=8916)}),_N_E=n.O()}]);
