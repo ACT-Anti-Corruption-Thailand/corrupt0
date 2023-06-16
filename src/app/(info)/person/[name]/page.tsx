802b60d433b589a083603bb4e04323ee364d6e38
rootEnginear <rootEnginear@users.noreply.github.com>
2023-06-16 11:32:32 +0700

    feat: add person card

--- src/app/(info)/person/[name]/page.tsx
+++ src/app/(info)/person/[name]/page.tsx
@@ -3,5 +3,15 @@ export async function generateStaticParams() {
 }
 
 export default function Person() {
-  return <div>Person</div>;
+  return (
+    <main>
+      <section className="flex flex-col gap-5 bg-white text-black text-center">
+        <span className="b6">
+          อัปเดตข้อมูลเมื่อวันที่ {new Date().toLocaleDateString("th")}
+        </span>
+        <span className="h2">สุชาติ ภิญโญ</span>
+        <div>LOL</div>
+      </section>
+    </main>
+  );
 }
