ed83eb6e30d46f1f021f0531479c2289e348185c
rootEnginear <rootEnginear@users.noreply.github.com>
2023-06-20 21:33:55 +0700

    feat: update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,21 +25,18 @@ export default function Footer({ className }: { className?: string }) {
         />
       </a>
       <div className="flex gap-20 text-balance">
-        <div className="flex flex-col gap-10">
-          <div>
-            <span className="block mb-5 font-bold">หน้าแรก</span>
-            <Link href="/">กลับไปที่หน้าแรก</Link>
-          </div>
-          <div>
-            <span className="block mb-5 font-bold">ข้อมูลบุคคล</span>
-            <Link href="/individual">ดูหน้าข้อมูลบุคคล</Link>
-          </div>
-          <div>
-            <span className="block mb-5 font-bold">ข้อมูลเงินบริจาค</span>
-            <Link href="/donation">ดูหน้าข้อมูลเงินบริจาค</Link>
-          </div>
-        </div>
-        <div>
+        <ul className="flex flex-col gap-10 font-bold">
+          <li>
+            <Link href="/">หน้าแรก</Link>
+          </li>
+          <li>
+            <Link href="/individual">ข้อมูลบุคคล</Link>
+          </li>
+          <li>
+            <Link href="/donation">ข้อมูลเงินบริจาค</Link>
+          </li>
+        </ul>
+        <div className="flex-1">
           <span className="block mb-5 font-bold">แหล่งข้อมูล</span>
           <ul className="flex flex-col gap-5">
             <li>
@@ -48,54 +45,132 @@ export default function Footer({ className }: { className?: string }) {
               </a>
             </li>
             <li>
-              <a href="/" target="_blank" rel="nofollow noopener noreferrer">
-                คณะกรรมการป้องกันและปราบปรามการทุจริตแห่งชาติ
+              <a
+                href="https://asset.nacc.go.th/dcs-app/"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                บัญชีทรัพย์สินหนี้สินฯ ป.ป.ช.
               </a>
             </li>
             <li>
-              <a href="/" target="_blank" rel="nofollow noopener noreferrer">
-                สำนักงานคณะกรรมการการเลือกตั้ง
+              <a
+                href="https://www.nacc.go.th/naccCulIncul/naccCulpability.php"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                คำชี้มูล ป.ป.ช.
               </a>
             </li>
             <li>
-              <a href="/" target="_blank" rel="nofollow noopener noreferrer">
-                They Work For Us
+              <a
+                href="http://www.supremecourt.or.th/division/แผนกคดีอาญาของผู้ดำรงตำแหน่งทางการเมือง/คำพิพากษาคดี"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                คำพิพากษาศาลฎีกา
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://market.sec.or.th/public/idisc/th/Enforce/Recent"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                การบังคับใช้กฎหมายของ ก.ล.ต.
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.ect.go.th/ect_th/news_all.php?cid=22"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                ข้อมูลเงินบริจาคพรรคการเมือง
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://ratchakitcha.soc.go.th"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                รายชื่อผู้บริหาร/ข้าราชการระดับสูง
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.soc.go.th/?page_id=538"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                รายชื่อข้าราชการการเมือง
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://theyworkforus.wevis.info"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                They Work for Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://creden.co/"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                ข้อมูลนิติบุคคล
               </a>
             </li>
           </ul>
         </div>
         <div>
           <span className="block mb-5 font-bold">ติดต่อ</span>
-          <ul className="flex gap-5">
+          <ul className="flex flex-col gap-[8px]">
             <li>
-              <a href="/" target="_blank" rel="nofollow noopener noreferrer">
+              <a
+                href="https://www.facebook.com/act.anticorruptionThailand"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
                 <Image
-                  className="w-10"
+                  className="w-20 h-20"
                   src="/logos/fb.svg"
-                  width={10}
-                  height={10}
+                  width={20}
+                  height={20}
                   alt="Facebook"
                 />
               </a>
             </li>
             <li>
-              <a href="/" target="_blank" rel="nofollow noopener noreferrer">
+              <a
+                href="https://twitter.com/ACT_Thailand"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
                 <Image
-                  className="w-10"
+                  className="w-20 h-20"
                   src="/logos/tw.svg"
-                  width={10}
-                  height={10}
+                  width={20}
+                  height={20}
                   alt="Twitter"
                 />
               </a>
             </li>
             <li>
-              <a href="/" target="_blank" rel="nofollow noopener noreferrer">
+              <a
+                href="http://www.anticorruption.in.th/2016/th/"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
                 <Image
-                  className="w-10"
-                  src="/logos/ig.svg"
-                  width={10}
-                  height={10}
+                  className="w-20 h-20"
+                  src="/icons/web.svg"
+                  width={20}
+                  height={20}
                   alt="Instagram"
                 />
               </a>
@@ -119,21 +194,21 @@ export default function Footer({ className }: { className?: string }) {
           src="/logos/act.png"
           width={15}
           height={15}
-          alt=""
+          alt="act"
         />
         <Image
           className="w-auto h-10"
           src="/logos/hand.svg"
           width={27.3}
           height={10}
-          alt=""
+          alt="hand"
         />
         <Image
           className="w-auto h-10"
           src="/logos/pu.svg"
           width={34.56}
           height={10}
-          alt=""
+          alt="punch up"
         />
       </div>
     </footer>
