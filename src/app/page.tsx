0d5e999b9d908e4e97fa9ca002fd87ac8fd8f43c
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2023-06-22 13:08:22 +0700

    refactor: img card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,18 +14,30 @@ export default function Home() {
       <Navbar floating />
       <Spotlight />
       <main className="text-center pt-50">
-        <p className="text-white text-30 mb-20 font-black ">มีอะไรให้ดูในเว็บไซต์นี้?</p>
+        <p className="text-white text-30 mb-20 font-black ">
+          มีอะไรให้ดูในเว็บไซต์นี้?
+        </p>
         <section className="py-20 px-10 bg-white rounded-10 mb-10">
-          <ImgCard
-            title="ดูข้อมูลนักการเมืองเเละเจ้าหน้าที่รัฐ"
-            topic={[
-              "สถานะทางการเงินเเละการยื่นบัญชีทรัพย์สินฯ",
-              "ความเกี่ยวข้องกับธุรกิจและโครงการภาครัฐ",
-              "เงินบริจาคพรรคการเมือง",
-              "ความเกี่ยวข้องกับคดีความ",
-            ]}
-            imgPath="/images/asset_politician.png"
-          />
+          <ImgCard imgPath="/images/asset_politician.png">
+            <div className="flex flex-row items-center justify-between">
+              <p className="text-32 self-start text-left">
+                ดูข้อมูลนักการเมืองเเละเจ้าหน้าที่รัฐ
+              </p>
+              <Image
+                className="w-[17px] h-[17px] -rotate-90"
+                src="./icons/arr-w.svg"
+                width={17}
+                height={17}
+                alt="arrow"
+              />
+            </div>
+            <ul className="list-inside list-disc text-left">
+              <li>สถานะทางการเงินเเละการยื่นบัญชีทรัพย์สินฯ</li>
+              <li>ความเกี่ยวข้องกับธุรกิจและโครงการภาครัฐ</li>
+              <li>เงินบริจาคพรรคการเมือง</li>
+              <li>ความเกี่ยวข้องกับคดีความ</li>
+            </ul>
+          </ImgCard>
           <p className="text-black text-24 my-10">ข้อมูลที่น่าสนใจ</p>
           <Slider>
             <Card
@@ -71,11 +83,23 @@ export default function Home() {
           </Slider>
         </section>
         <section className="py-20 px-10 bg-white rounded-10 mb-10">
-          <ImgCard
-            title="ดูข้อมูลเงินบริจาคพรรคการเมือง"
-            topic={["ยอดบริจาคที่น่าสังเกตของบุคคล/นิติบุคคล"]}
-            imgPath="/images/asset_donation.png"
-          />
+          <ImgCard imgPath="/images/asset_donation.png">
+          <div className="flex flex-row items-center justify-between">
+              <p className="text-32 self-start text-left">
+                ดูข้อมูลเงินบริจาคพรรคการเมือง
+              </p>
+              <Image
+                className="w-[17px] h-[17px] -rotate-90"
+                src="./icons/arr-w.svg"
+                width={17}
+                height={17}
+                alt="arrow"
+              />
+            </div>
+            <ul className="list-inside list-disc text-left">
+              <li>ยอดบริจาคที่น่าสังเกตของบุคคล/นิติบุคคล</li>
+            </ul>
+          </ImgCard>
           <p className="text-black text-24 my-10">ข้อมูลที่น่าสนใจ</p>
           <Slider>
             <Card
@@ -130,8 +154,8 @@ export default function Home() {
         <section className="mb-20 b5 px-10">
           <p className="mb-20">
             ความโปร่งใสทางการเมือง (Political Transparency)
-            คือการเปิดโอกาสให้ประชาชนทุกคนเข้าถึงและตั้งคำถาม เกี่ยวกับที่มา อำนาจ
-            และการปฏิบัติหน้าที่ ของนักการเมืองและเจ้าหน้าที่รัฐได้
+            คือการเปิดโอกาสให้ประชาชนทุกคนเข้าถึงและตั้งคำถาม เกี่ยวกับที่มา
+            อำนาจ และการปฏิบัติหน้าที่ ของนักการเมืองและเจ้าหน้าที่รัฐได้
           </p>
           <p className="mb-20">
             ACT Ai จึงถูกพัฒนาขึ้น เพื่อเป็นฐานข้อมูลความโปร่งใส
@@ -140,8 +164,9 @@ export default function Home() {
             และผลประโยชน์ของส่วนรวมร่วมกัน
           </p>
           <p>
-            นอกจากนี้ คุณยังสามารถช่วยสร้างฐานข้อมูลนี้ ด้วยการช่วยแปลงข้อมูลเป็นดิจิทัล
-            ข้อมูลในฐานข้อมูล ความโปร่งใสของ ACT Ai
+            นอกจากนี้ คุณยังสามารถช่วยสร้างฐานข้อมูลนี้
+            ด้วยการช่วยแปลงข้อมูลเป็นดิจิทัล ข้อมูลในฐานข้อมูล ความโปร่งใสของ
+            ACT Ai
           </p>
           <div
             style={
@@ -152,7 +177,9 @@ export default function Home() {
             className="my-30 py-10 px-20 rounded-10 relative bg-cover bg-center bg-no-repeat flex flex-col text-white justify-start mx-20"
           >
             <div className="flex flex-row justify-center items-center p-20">
-              <p className="text-32 self-start text-left">ร่วมแปลงข้อมูลเป็นดิจิตอล</p>
+              <p className="text-32 self-start text-left">
+                ร่วมแปลงข้อมูลเป็นดิจิตอล
+              </p>
               <Image
                 src="/icons/external-w.svg"
                 className="w-[25px] h-[22px] pl-10"
--- src/components/ImgCard.tsx
+++ src/components/ImgCard.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
 
 interface ImgCardProps {
-  title: string;
   imgPath: string;
-  topic: Array<string>;
+  children?: React.ReactNode;
 }
 
 const ImgCard = (props: ImgCardProps) => {
@@ -16,21 +15,7 @@ const ImgCard = (props: ImgCardProps) => {
       }
       className="my-16 py-10 px-20 rounded-10 relative bg-cover flex flex-col text-white justify-start"
     >
-      <div className="flex flex-row">
-        <p className="text-32 self-start text-left">{props.title}</p>
-        <Image
-          className="ml-30 w-[17px] h-[17px] -rotate-90"
-          src="/icons/arr-w.svg"
-          width={17}
-          height={17}
-          alt=""
-        />
-      </div>
-      <ul className="list-inside list-disc text-left">
-        {props.topic.map((topic, index) => {
-          return <li key={index}>{topic}</li>;
-        })}
-      </ul>
+      {props.children}
     </div>
   );
 };
