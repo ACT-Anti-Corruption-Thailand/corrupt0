f31ab7ab080bd692cf25fba64804071f2b612324
rootEnginear <rootEnginear@users.noreply.github.com>
2023-06-22 18:15:18 +0700

    fix: change from unovis to recharts

--- src/components/PersonChart.tsx
+++ src/components/PersonChart.tsx
@@ -1,27 +1,78 @@
 "use client";
 
-import { VisAxis, VisLine, VisXYContainer } from "@unovis/react";
-import { CurveType } from "@unovis/ts";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+  Label,
+} from "recharts";
 
-type DataRecord = { x: number; y1: number; y2: number };
+type DataRecord = { x: string; y1: number; y2: number };
 const data: DataRecord[] = [
-  { x: 2558, y1: 1, y2: 3 },
-  { x: 2559, y1: 2, y2: 2 },
-  { x: 2560, y1: 3, y2: 1 },
-  { x: 2561, y1: 4, y2: 2 },
-  { x: 2562, y1: 3, y2: 3 },
-  { x: 2563, y1: 2, y2: 4 },
+  { x: "2558", y1: 1, y2: 3 },
+  { x: "2559", y1: 2, y2: 2 },
+  { x: "2560", y1: 3, y2: 1 },
+  { x: "2561", y1: 4, y2: 2 },
+  { x: "2562", y1: 3, y2: 3 },
+  { x: "2563", y1: 2, y2: 4 },
 ];
 
-export default function PersonChart() {
-  const x = (d: DataRecord) => d.x;
-  const y = [(d: DataRecord) => d.y1, (d: DataRecord) => d.y2];
+const XAxisLabel = (props: { x: number; y: number; payload: { value: any } }) => {
+  const { x, y, payload } = props;
 
   return (
-    <VisXYContainer data={data} height={150}>
-      <VisLine x={x} y={y} curveType={CurveType.Linear} />
-      <VisAxis type="x" numTicks={data.length} tickLine={undefined} />
-      <VisAxis type="y" tickLine={undefined} />
-    </VisXYContainer>
+    <text x={x} y={y + 6} className="b7 fill-white" textAnchor="middle">
+      {payload.value}
+    </text>
+  );
+};
+
+const YAxisLabel = (props: { x: number; y: number; payload: { value: any } }) => {
+  const { x, y, payload } = props;
+
+  return (
+    <text x={x - 2} y={y} className="b7 fill-gray-5" dominantBaseline="middle">
+      {payload.value}
+    </text>
+  );
+};
+
+export default function PersonChart() {
+  return (
+    <ResponsiveContainer height={150}>
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{ top: 40, right: 10, bottom: 0, left: -40 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          dataKey="x"
+          padding={{ left: 30, right: 30 }}
+          tickLine={false}
+          tick={XAxisLabel}
+        >
+          <Label className="b7" value="ปี" offset={0} position="right" />
+        </XAxis>
+        <YAxis tickLine={false} tick={YAxisLabel}>
+          <Label
+            className="b7"
+            offset={10}
+            position="top"
+            value="จำนวนเงิน (ล้านบาท)"
+            width={10}
+            style={{
+              transform: "translateX(40px)",
+            }}
+          />
+        </YAxis>
+        <Line dataKey="y1" stroke="#8884d8" isAnimationActive={false} type="linear" />
+        <Line dataKey="y2" stroke="#82ca9d" isAnimationActive={false} type="linear" />
+      </LineChart>
+    </ResponsiveContainer>
   );
 }
