f08f0043668cc837a351a21d20a16efac33600a5
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2023-06-27 00:18:53 +0700

    feat: money parser

---
+++ src/functions/moneyFormatter.ts
@@ -0,0 +1,17 @@
+export const moneyFormatter = (value: number) => {
+    if(value > 1000000000){
+        return (value/1000000000).toFixed(2).toString() + 'B';
+      }else if(value > 1000000){
+        return (value/1000000).toFixed(2).toString() + 'M';
+      }else if(value > 1000){
+        return (value/1000).toFixed(2).toString() + 'K';
+      }else{
+        return value.toString();
+      }
+}
+
+export const thaiMoneyFormatter = (value: number) => {
+    if(value > 1000000) {
+        return (value/1000000).toFixed(2).toString() + ' ล้านบาท';
+    }
+}
