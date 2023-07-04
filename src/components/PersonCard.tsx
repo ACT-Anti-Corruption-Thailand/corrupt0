68cc6a34eb65dff8c869a43b2330c9f9c4c1f766
wit03 <witjarukit@gmail.com>
2023-07-05 00:39:11 +0700

    fix: graph styling

--- src/components/PersonCard.tsx
+++ src/components/PersonCard.tsx
@@ -17,32 +17,35 @@ const PersonCard = (props: PersonCardProps) => {
       .toString() + "%";
 
   return (
-    <div className="bg-white bg-opacity-10 text-gray-4 rounded-5 py-10 px-5 lg:px-20 lg:py-20 my-10 flex gap-5 items-start text-18 w-full">
-      <Image src={props.imgPath} width={40} height={40} alt="person" className="h-[25px] lg:h-40"/>
-      <div className="flex flex-col w-full">
-        <div className="flex justify-between text-gray-2">
-          <p className="b3">{props.name}</p>
-          <p className="b4">{money}</p>
+    <div className="flex flex-col bg-white bg-opacity-10 text-gray-4 rounded-5 py-10 px-5 lg:px-20 lg:py-20 my-10 text-18 w-full">
+      <div className="flex gap-5 items-start">
+        <Image src={props.imgPath} width={40} height={40} alt="person" className="h-[25px] lg:h-40" />
+        <div className="flex flex-col w-full">
+          <div className="flex justify-between text-gray-2">
+            <p className="b3">{props.name}</p>
+            <p className="b4">{money}</p>
+          </div>
+          <div className="flex justify-between text-right b6">
+            <p>{props.title}</p>
+            <p>{unit}</p>
+          </div>
+
         </div>
-        <div className="flex justify-between text-right b6">
-          <p>{props.title}</p>
-          <p>{unit}</p>
-        </div>
-        <div
-          style={
-            {
-              "--bar": bar,
-            } as React.CSSProperties
-          }
-          className="h-10 lg:h-20 w-[var(--bar)] bg-white mt-10"
+        <Image
+          className="-rotate-90 ml-4 h-10 w-12 lg:h-[23px] lg:w-[27px]"
+          src="/icons/arr-g.svg"
+          width={12}
+          height={10}
+          alt="arrow"
         />
       </div>
-      <Image
-        className="-rotate-90 ml-4"
-        src="/icons/arr-g.svg"
-        width={12}
-        height={10}
-        alt="arrow"
+      <div
+        style={
+          {
+            "--bar": bar,
+          } as React.CSSProperties
+        }
+        className="h-10 lg:h-20 w-[var(--bar)] bg-white mt-10"
       />
     </div>
   );
