925db66795454c248488521109899c192edaf78f
wit03 <witjarukit@gmail.com>
2023-07-05 02:50:14 +0700

    feat: donation page

--- src/app/(info)/donation/page.tsx
+++ src/app/(info)/donation/page.tsx
@@ -1,3 +1,92 @@
+"use client";
+
+import React from "react";
+import ImgCard from "@/components/ImgCard";
+import Image from "next/image";
+import PersonCard from "@/components/PersonCard";
+import Search from "@/components/Search";
+
+import GraphCard from "@/components/GraphCard";
+import { thaiMoneyFormatter } from "@/functions/moneyFormatter";
+
 export default function Donation() {
-  return <div>Donation</div>;
+  const [selected, setSelected] = React.useState("");
+
+  const people = [
+    { id: 1, name: "Wade Cooper", title: "Regional Paradigm Technician" },
+    { id: 2, name: "Arlene Mccoy", title: "Hello" },
+    { id: 3, name: "Devon Webb", title: "jasf" },
+    { id: 4, name: "Tom Cook", title: "asf" },
+    { id: 5, name: "Tanya Fox", title: "asf" },
+    { id: 6, name: "Hellen Schmidt", title: "asf" },
+  ];
+
+  return (
+    <>
+      <section className="flex flex-col items-center">
+        <ImgCard imgPath="/images/asset_donation.png">
+          <div className="flex flex-col justify-center my-auto py-30 lg:mx-[15vw] xl:mx-[25vw] lg:p-[70px]">
+            <Image
+              className="self-center mb-10 h-[45px] lg:h-100"
+              src="./icons/donate.svg"
+              width={100}
+              height={100}
+              alt="financial"
+            />
+            <p className="font-black text-40 text-center lg:h1">
+              ดูข้อมูลเงินบริจากพรรคการเมือง
+            </p>
+          </div>
+
+        </ImgCard>
+        <p className="text-gray-5 text-18 mt-10 lg:mt-30">อัพเดทข้อมูลเมื่อวันที่ 00/00/2556</p>
+        <div className="flex justify-center items-center gap-10 bg-gray-6 w-screen py-10 my-10 lg:py-15 lg:my-30 text-24 lg:h3">
+          <Image src="./icons/borrow.svg" width={60} height={60} alt="borrow" className="h-30 w-30 lg:h-60 lg:w-60" />
+          พรรคที่ได้รับเงินบริจาค
+        </div>
+        <Search
+          placeholder="ค้นหาด้วยชื่อ/นามสกุล"
+          data={people}
+          selected={people[0]}
+          setSelected={setSelected}
+        />
+        <div className="flex flex-col items-center text-center text-18 lg:b4 pb-10 lg:pb-30 w-[90vw] min-w-[300px] max-w-[850px]">
+          <PersonCard
+            name="สุชาติ ภิญโญ"
+            title="สมาชิกวุฒิสภา"
+            amount={1637239}
+            maxAmount={10000000}
+            imgPath='/icons/person.svg'
+          />
+          <PersonCard
+            name="สุชาติ ภิญโญ"
+            title="สมาชิกวุฒิสภา"
+            amount={1637239}
+            maxAmount={10000000}
+            imgPath='/icons/person.svg'
+          />
+        </div>
+        <div className="flex justify-center items-center gap-10 bg-gray-6 w-screen py-10 my-10 lg:py-15 lg:my-30 text-24 lg:h3">
+          <Image src="./icons/donate.svg" width={60} height={60} alt="borrow" className="h-30 w-30 lg:h-60 lg:w-60" />
+          พรรคที่ได้รับเงิน
+        </div>
+        <div className="flex flex-col items-center text-center text-18 lg:b4 pb-10 lg:pb-30 w-[90vw] min-w-[300px] max-w-[850px]">
+          <PersonCard
+            name="สุชาติ ภิญโญ"
+            title="สมาชิกวุฒิสภา"
+            amount={1637239}
+            maxAmount={10000000}
+            imgPath='/icons/person.svg'
+          />
+          <PersonCard
+            name="สุชาติ ภิญโญ"
+            title="สมาชิกวุฒิสภา"
+            amount={1637239}
+            maxAmount={10000000}
+            imgPath='/icons/person.svg'
+          />
+        </div>
+      </section>
+    </>
+  );
 }
