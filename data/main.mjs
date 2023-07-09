0709d5d29bebd024e23afab0aeeb5c32290be9c8
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2023-07-09 16:53:49 +0700

    feat: color format

--- data/main.mjs
+++ data/main.mjs
@@ -1,3 +1,5 @@
+import { generatePartyAssets } from "./functions/partyAssets.mjs";
 import { generateNames } from "./functions/politician.mjs";
 
 await generateNames();
+await generatePartyAssets();
