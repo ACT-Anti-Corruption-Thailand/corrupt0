f2972d53f5246f5fa83a442698480d14d278d9c1
rootEnginear <rootEnginear@users.noreply.github.com>
2023-07-09 18:34:47 +0700

    fix: fetch handling duplicates

--- data/fetch.mjs
+++ data/fetch.mjs
@@ -1,6 +1,8 @@
-import fs from "fs/promises";
+import fs, { constants } from "fs/promises";
 import path from "path";
 import JSON5 from "json5";
+
+const DIRECTORY_PATH = "data/raw";
 
 const FILES = [
   "https://storage.googleapis.com/act_datacatalog/corrupt0/co_003/001/pdf/csv/nacc.csv",
@@ -10,7 +12,6 @@ const FILES = [
   "https://storage.googleapis.com/act_datacatalog/corrupt0/co_004/001/ect/co004_opendata_path.json",
   "https://storage.googleapis.com/act_datacatalog/corrupt0/co_002/001/sec/sec.csv",
   "https://storage.googleapis.com/act_opendata/opendata/master_data/ds_002/001/ds002_opendata_path.json",
-  "https://storage.googleapis.com/act_opendata/opendata/master_data/ds_002/001/ds002_opendata_path.json",
   "https://storage.googleapis.com/act_datacatalog/master_data/ds_003/001/person_family_opendata/person_family.csv",
   "https://storage.googleapis.com/act_datacatalog/master_data/ds_003/001/political_office_holder_opendata/political_office_holder.csv",
   "https://storage.googleapis.com/act_datacatalog/master_data/ds_004/001/nacc/nacc_culpability.csv",
@@ -19,25 +20,53 @@ const FILES = [
   "https://storage.googleapis.com/act_datacatalog/master_data/ds_009/001/ds009_opendata_path.json",
 ];
 
+export const removeExistedData = async () => {
+  console.info("ℹ Removing Old Files");
+
+  for (const file of await fs.readdir(DIRECTORY_PATH)) {
+    await fs.unlink(path.join(DIRECTORY_PATH, file));
+  }
+};
+
 export const fetchData = async (files) => {
-  fs.mkdir("data/raw", { recursive: true });
+  console.info("ℹ Fetching...");
+
+  fs.mkdir(DIRECTORY_PATH, { recursive: true });
+
   for (let i = 0; i < files.length; i++) {
     const resp = await fetch(files[i]);
     const text = await resp.text();
-    await fs.writeFile(`data/raw/${files[i].split("/").at(-1)}`, text);
+
+    let output = `${DIRECTORY_PATH}/${files[i].split("/").at(-1)}`;
+
+    for (let j = 0; ; j++) {
+      try {
+        await fs.access(output, constants.F_OK); // Throw error when cannot access
+
+        // Can access -> duplicate file -> rename
+        const [ext, ...rest] = output.split(".").reverse();
+        output = `${rest.reverse().join(".")}${j}.${ext}`;
+        continue;
+      } catch (e) {
+        // Cannot access -> Not existed
+        await fs.writeFile(output, text);
+      }
+      break;
+    }
   }
 };
 
 export const fetchSubData = async () => {
-  let subFetchList = [];
-  const directoryPath = "data/raw";
+  console.info("ℹ Fetching Subdata...");
 
-  const files = await fs.readdir(directoryPath);
+  let subFetchList = [];
+
+  const files = await fs.readdir(DIRECTORY_PATH);
   const jsonFiles = files.filter((file) => path.extname(file).toLowerCase() === ".json");
 
   for (let i = 0; i < jsonFiles.length; i++) {
     const file = jsonFiles[i];
-    const filePath = path.join(directoryPath, file);
+    const filePath = path.join(DIRECTORY_PATH, file);
     const fileContent = await fs.readFile(filePath, "utf8");
 
     subFetchList[i] = Object.values(JSON5.parse(fileContent));
@@ -46,5 +75,6 @@ export const fetchSubData = async () => {
   await fetchData(subFetchList.flat(Infinity));
 };
 
+await removeExistedData();
 await fetchData(FILES);
 await fetchSubData();
