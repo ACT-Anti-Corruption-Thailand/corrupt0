1c1a356e75229497a82e590e7edf3a024060db78
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2023-07-10 03:09:56 +0700

    feat: totalPerYear and partyPerYear Donation data

--- data/functions/donation.mjs
+++ data/functions/donation.mjs
@@ -2,53 +2,63 @@ import fs from "fs/promises";
 import path from "path";
 import * as aq from "arquero";
 
-const directoryPath = "data/raw";
-const files = await fs.readdir(directoryPath);
+const generateTotalDonation = async () => {
+  const directoryPath = "data/raw";
+  const files = await fs.readdir(directoryPath);
 
-const fileLists = files
-  .filter((file) => file.toLowerCase().includes("political_party_donor"))
-  .map((file) => path.join(directoryPath, file));
+  const fileLists = files
+    .filter((file) => file.toLowerCase().includes("political_party_donor"))
+    .map((file) => path.join(directoryPath, file));
 
-const readCSV = async (filePath) => {
-  const table = await aq.loadCSV(filePath);
-  return table;
+  const readCSV = async (filePath) => {
+    const table = await aq.loadCSV(filePath);
+    return table;
+  };
+
+  const createTable = async (filePaths) => {
+    let tables = [];
+    for (let file of filePaths) {
+      tables.push(await readCSV(file));
+    }
+    const concatenatedTable = tables.reduce((all, curr) => all.concat(curr));
+    return concatenatedTable;
+  };
+
+  const table = await createTable(fileLists);
+
+  const totalPerYearTable = table
+    .select("year", "amount")
+    .groupby("year")
+    .rollup({ total: (d) => op.sum(d.amount) })
+    .orderby(aq.desc((d) => d.total))
+    .objects();
+
+  const partyPerYearTable = table
+    .select("party", "year", "amount")
+    .objects()
+    .reduce((acc, obj) => {
+        const { year, amount, ...rest } = obj;
+        if (!acc[year]) {
+          acc[year] = [];
+        }
+      
+        const existingParty = acc[year].find(party => party.party === obj.party);
+        if (existingParty) {
+          existingParty.amount += amount;
+        } else {
+          acc[year].push({ party: obj.party, amount });
+        }
+        
+        return acc;
+      }, {});
+
+    return { totalPerYearTable, partyPerYearTable }
 };
 
-const createTable = async (filePaths) => {
-  let tables = [];
-  for (let file of filePaths) {
-    tables.push(await readCSV(file));
-  }
-  const concatenatedTable = tables.reduce((all, curr) => all.concat(curr));
-  return concatenatedTable;
-};
 
-const allTable = await createTable(fileLists);
-allTable.print();
-
-/*
-Ideal Data;
-
-const PartyDonation = [
-    2565: {
-        totalAmount: 10000,
-        data: [
-            {
-                name: "พลังประชารัฐ",
-                amount: 10000
-            },
-            {
-                name: "พลังประชาธิปไตย",
-                amount: 0
-            }
-        ]
-    }
-    2564: {
-        ...
-    }
-]
-
-const IndividualDonor = [
-
-];
-*/
+export const generateDonation = async () => {
+    const Donation = await generateTotalDonation();
+    fs.mkdir("src/data/donation", { recursive: true });
+    await fs.writeFile("src/data/donation/totalPerYear.json", JSON.stringify(Donation.totalPerYearTable))
+    await fs.writeFile("src/data/donation/partyPerYear.json", JSON.stringify(Donation.partyPerYearTable))
+}
--- data/main.mjs
+++ data/main.mjs
@@ -2,9 +2,11 @@ import fs from "fs";
 import { generatePolitician } from "./functions/politician.mjs";
 import { generatePartyAssets } from "./functions/partyAssets.mjs";
 import { generatePoliticianImages } from "./functions/politicianImages.mjs";
+import { generateDonation } from "./functions/donation.mjs";
 
 fs.mkdirSync("src/data/info", { recursive: true });
 
 await generatePolitician();
 await generatePartyAssets();
+await generateDonation();
 generatePoliticianImages();
