dbddb5cfc7f0cf0c7311ee261fef180eff056db7
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2023-07-11 19:44:00 +0700

    feat: ทุกปี in each donation data

--- data/functions/totalDonation.mjs
+++ data/functions/totalDonation.mjs
@@ -11,17 +11,17 @@ const getTotalDonation = async () => {
     .groupby("year")
     .rollup({ total: (d) => op.sum(d.amount) })
     .orderby(aq.desc((d) => d.total))
-    .objects();
 
-  const partyPerYearTable = table
-    .select("party", "year", "amount")
-    .rename({ amount: "_amount" })
-    .groupby("year", "party")
-    .rollup({ amount: (d) => op.sum(d._amount) })
+  const totalTable = table
+    .select("amount")
+    .rollup({ total: (d) => op.sum(d.amount) })
+    .derive({ year: () => "ทุกปี" });
+
+  const totalPerYearWithTotalTable = totalTable
+    .concat(totalPerYearTable)
     .objects()
     .reduce((acc, obj) => {
       const { year } = obj;
-
       const objWOYear = { ...obj };
       delete objWOYear.year;
 
@@ -34,7 +34,38 @@ const getTotalDonation = async () => {
       return acc;
     }, {});
 
-  return { totalPerYearTable, partyPerYearTable };
+
+
+  const partyPerYearTable = table
+    .select("party", "year", "amount")
+    .rename({ amount: "_amount" })
+    .groupby("year", "party")
+    .rollup({ amount: (d) => op.sum(d._amount) });
+
+  const totalPerPartyTable = table
+    .select("party", "amount")
+    .groupby("party")
+    .rollup({ amount: (d) => op.sum(d.amount) })
+    .derive({ year: () => "ทุกปี" });
+
+  const partyPerYearWithTotalTable = totalPerPartyTable
+    .concat(partyPerYearTable)
+    .objects()
+    .reduce((acc, obj) => {
+      const { year } = obj;
+      const objWOYear = { ...obj };
+      delete objWOYear.year;
+
+      if (year in acc) {
+        acc[year].push(objWOYear);
+      } else {
+        acc[year] = [objWOYear];
+      }
+
+      return acc;
+    }, {});
+
+  return { totalPerYearWithTotalTable, partyPerYearWithTotalTable };
 };
 
 export const generateTotalDonation = async () => {
@@ -43,11 +74,11 @@ export const generateTotalDonation = async () => {
   fs.mkdir("src/data/donation", { recursive: true });
 
   await fs.writeFile(
-    "src/data/donation/totalPerYear.json",
-    JSON.stringify(Donation.totalPerYearTable)
+    "src/data/donation/totalPerYearWithTotal.json",
+    JSON.stringify(Donation.totalPerYearWithTotalTable)
   );
   await fs.writeFile(
-    "src/data/donation/partyPerYear.json",
-    JSON.stringify(Donation.partyPerYearTable)
+    "src/data/donation/partyPerYearWithTotal.json",
+    JSON.stringify(Donation.partyPerYearWithTotalTable)
   );
 };
