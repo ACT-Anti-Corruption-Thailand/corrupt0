5df70346bfd01c3e90163c7a7423864af56ccfcb
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2023-07-13 13:29:41 +0700

    fix: change party assets format

--- data/functions/partyAssets.mjs
+++ data/functions/partyAssets.mjs
@@ -2,20 +2,15 @@ import fs from "fs/promises";
 import data from "../constants/partyAssets.json" assert { type: "json" };
 
 export const generatePartyAssets = async () => {
-  let party = data.list
-    .filter((e) => e.PartyType === "พรรค")
-    .map(({ Name, Color, Images }) => ({
-      Name,
-      Color,
-      Images,
-    }));
-  party.map(({ Images }) => {
-    if (Images && Images[0].path) {
-      Images[0].path = "https://sheets.wevis.info/" + Images[0].path;
-      const { path: url, ...rest } = Images[0];
-      Images[0] = { url, ...rest };
-    }
-  });
+  let party = Object.fromEntries(data.list.map(e => [e.Name, {
+    color: e.Color,
+    image: e.Images
+    ? e.Images[0].url
+      ? e.Images[0].url
+      : `https://sheets.wevis.info/${e.Images[0].path}`
+    : null,
+  }]))
+
   await fs.mkdir("src/data/color", { recursive: true });
   await fs.writeFile(`src/data/color/partyAssets.json`, JSON.stringify(party));
 };
