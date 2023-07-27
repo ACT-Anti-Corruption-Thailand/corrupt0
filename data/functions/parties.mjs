4384b3989a7a17c00bcefdb5784e91695d630e57
rootEnginear <rootEnginear@users.noreply.github.com>
2023-07-27 18:13:48 +0700

    fix: dedupe parties

--- data/functions/parties.mjs
+++ data/functions/parties.mjs
@@ -4,11 +4,14 @@ import { op } from "arquero";
 import * as aq from "arquero";
 
 const RAW_DONATION_TABLE = await getDonationData();
-const DONATION_TABLE = RAW_DONATION_TABLE
-  .derive({ year: (d) => op.parse_int(d.year + 543) })
-  .derive({
-    donor_fullname: (d) => op.equal(d.donor_prefix, "นิติบุคคล") ? d.donor_fullname : op.replace(d.donor_firstname + " " + d.donor_lastname, /\s+|\/|\\/g, " ")
-  })
+const DONATION_TABLE = RAW_DONATION_TABLE.derive({
+  year: (d) => op.parse_int(d.year + 543),
+}).derive({
+  donor_fullname: (d) =>
+    op.equal(d.donor_prefix, "นิติบุคคล")
+      ? d.donor_fullname
+      : op.replace(d.donor_firstname + " " + d.donor_lastname, /\s+|\/|\\/g, " "),
+});
 
 // ██████╗  █████╗ ██████╗ ████████╗██╗███████╗███████╗
 // ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██║██╔════╝██╔════╝
@@ -18,24 +21,36 @@ const DONATION_TABLE = RAW_DONATION_TABLE
 // ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝╚══════╝╚══════╝
 
 export const getPartiesFromDonation = async () => {
-  return DONATION_TABLE.select("party")
-    .dedupe()
-    .objects()
-    .map(
-      (e) => (e.party.includes("พรรค") ? "" : "พรรค") + e.party.replace(/\s+|\/|\\/g, "-")
-    );
+  return [
+    ...new Set(
+      DONATION_TABLE.select("party")
+        .dedupe()
+        .objects()
+        .map(
+          (e) =>
+            (e.party.includes("พรรค") ? "" : "พรรค") + e.party.replace(/\s+|\/|\\/g, "-")
+        )
+    ),
+  ];
 };
 
 export const getPartyDonor = async (party) => {
-  return DONATION_TABLE.select("party", "month", "year", "donor_prefix", "donor_fullname", "amount")
-    .filter(aq.escape(d => d.party === party))
+  return DONATION_TABLE.select(
+    "party",
+    "month",
+    "year",
+    "donor_prefix",
+    "donor_fullname",
+    "amount"
+  )
+    .filter(aq.escape((d) => d.party === party))
     .objects()
-    .map(obj => {
-      const { party, ...rest } = obj
-      return rest
+    .map((obj) => {
+      const { party, ...rest } = obj;
+      return rest;
     })
-    .sort((a, b) => b.amount - a.amount)
-}
+    .sort((a, b) => b.amount - a.amount);
+};
 
 // ███╗   ███╗ █████╗ ██╗███╗   ██╗
 // ████╗ ████║██╔══██╗██║████╗  ██║
@@ -49,7 +64,7 @@ export const generateParties = async () => {
 
   await fs.writeFile(`src/data/parties.json`, JSON.stringify(parties));
   for (let party of parties) {
-    const donor = await getPartyDonor(party.replace("พรรค", ""))
+    const donor = await getPartyDonor(party.replace("พรรค", ""));
     await fs.writeFile(`src/data/info/${party}.json`, JSON.stringify(donor));
   }
 };
