9f2461de4c762f151011f428f5112ec40d2f7db7
rootEnginear <rootEnginear@users.noreply.github.com>
2023-08-16 01:15:18 +0700

    fix: update fetch path

--- data/fetch.mjs
+++ data/fetch.mjs
@@ -6,16 +6,16 @@ const DIRECTORY_PATH = path.join("data", "raw");
 
 const FILES = [
   "https://storage.googleapis.com/act_datacatalog/corrupt0/co_003/001/pdf/csv/nacc.csv",
-  "https://storage.googleapis.com/act_datacatalog/corrupt0/co_003/001/nacc/co003_opendata_path.json",
   "https://storage.googleapis.com/act_datacatalog/corrupt0/co_001/001/nacc_culpability/nacc_culpability.csv",
   "https://storage.googleapis.com/act_datacatalog/corrupt0/co_004/001/pdf/ect.csv",
-  "https://storage.googleapis.com/act_datacatalog/corrupt0/co_004/001/ect/co004_opendata_path.json",
   "https://storage.googleapis.com/act_datacatalog/corrupt0/co_002/001/sec/sec.csv",
-  "https://storage.googleapis.com/act_opendata/opendata/master_data/ds_002/001/ds002_opendata_path.json",
   "https://storage.googleapis.com/act_datacatalog/master_data/ds_003/001/person_family_opendata/person_family.csv",
   "https://storage.googleapis.com/act_datacatalog/master_data/ds_003/001/political_office_holder_opendata/political_office_holder.csv",
   "https://storage.googleapis.com/act_datacatalog/master_data/ds_005/001/judgement/judgement.csv",
   "https://storage.googleapis.com/act_datacatalog/master_data/ds_007/001/public_sector_high_ranking_officer/public_sector_high_ranking_officer.csv",
+  "https://storage.googleapis.com/act_datacatalog/corrupt0/co_003/001/nacc/co003_opendata_path.json",
+  "https://storage.googleapis.com/act_datacatalog/corrupt0/co_004/001/ect/co004_opendata_path.json",
+  "https://storage.googleapis.com/act_opendata/opendata/master_data/ds_002/001/ds002_opendata_path.json",
   "https://storage.googleapis.com/act_datacatalog/master_data/ds_009/001/ds009_opendata_path.json",
 ];
 
@@ -57,7 +57,6 @@ export const fetchData = async (files) => {
     } catch (e) {
       console.error(`🛑 Error occurred in ${files[i]}: ${e.message}`);
       console.error(`🛑 Skipping...`);
-      subFetchList[i] = [];
     }
   }
 };
@@ -76,7 +75,17 @@ export const fetchSubData = async () => {
     const fileContent = await fs.readFile(filePath, "utf8");
 
     try {
-      subFetchList[i] = Object.values(JSON5.parse(fileContent));
+      const parsedContent = JSON5.parse(fileContent);
+      switch (file) {
+        case "co003_opendata_path.json":
+        case "co004_opendata_path.json":
+          subFetchList[i] = parsedContent.database_format;
+          break;
+        case "ds002_opendata_path.json":
+        case "ds009_opendata_path.json":
+        default:
+          subFetchList[i] = Object.values(parsedContent);
+      }
     } catch (e) {
       console.error(`🛑 Error occurred in ${file}: ${e.message}`);
       console.error(`🛑 Skipping...`);
