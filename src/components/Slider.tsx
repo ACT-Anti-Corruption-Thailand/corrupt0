d1f2cac6661eaba5e95cb0b06f10d3992b0fe649
rootEnginear <rootEnginear@users.noreply.github.com>
2023-08-17 03:23:43 +0700

    fix: slider drag

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,14 +1,26 @@
 "use client";
-import type { ReactNode } from "react";
+import { useRef } from "react";
+import { useDraggable } from "react-use-draggable-scroll";
+
+import type { MutableRefObject, ReactNode } from "react";
 
 interface SliderProps {
   children: ReactNode;
 }
 
-const Slider = ({ children }: SliderProps) => (
-  <div className="overflow-x-auto pb-10 carousel-scrollbar flex-1">
-    <div className="flex gap-10 w-max mx-auto h-full">{children}</div>
-  </div>
-);
+const Slider = ({ children }: SliderProps) => {
+  const elSlider = useRef<HTMLDivElement>() as MutableRefObject<HTMLInputElement>;
+  const { events } = useDraggable(elSlider);
+
+  return (
+    <div
+      ref={elSlider}
+      {...events}
+      className="overflow-x-auto pb-10 carousel-scrollbar flex-1 cursor-grab"
+    >
+      <div className="flex gap-10 w-max mx-auto h-full">{children}</div>
+    </div>
+  );
+};
 
 export default Slider;
